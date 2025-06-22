d02191d0dcb45ede887f12c7609750c7f6d8df07
AACXX <a08001212@gmail.com>
2025-06-22 14:32:46 +0800

    Finish ch1-1 homework.

---
+++ ch1/ch1-1/README.md
@@ -0,0 +1,11 @@
+## 实现一个linux应用程序A，显示当前目录下的文件名。（用C或Rust编程）
+
+實作一個類似 Linux ls 指令的程式，當執行 ls 或 ls <路徑> 時，能顯示指定目錄下的檔案名稱。
+
+```=bash
+cargo run # like ls
+```
+
+```=bash
+cargo run <path> # like ls <path>
+```
--- ch1/ch1-1/src/main.rs
+++ ch1/ch1-1/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let dir_path = if args.len() >= 2 {&args[1]} else {"./"}; 
+    let paths = fs::read_dir(dir_path).unwrap();
+
+    for path in paths {
+        if let Some(list_file_name) = path.unwrap().path().file_name() {
+            println!("{}", list_file_name.to_string_lossy());
+        }
+
+    }
 }
